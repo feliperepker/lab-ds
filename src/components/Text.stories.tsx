1ac4bc864c1534ed256d3083f87f0514a76af6c6
Felipe Repker <102967559+feliperepker@users.noreply.github.com>
2022-10-13 08:16:39 -0300

    First commit

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,47 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Text, TextProps } from './Text';
+
+export default { 
+  title: 'Components/Text',
+  component: Text,
+  args: {
+    children: 'Lorem ipsum',
+    size:'md',
+  }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm'
+  }
+}
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: 'lg'
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type:'inline-radio'
+      }
+    }
+  }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    asChild:true,
+    children: (
+      <p>Text with P tag</p>
+    )
+  },
+  argTypes: {
+    children: {
+      table: {
+        disabled: true
+      }
+    }
+  }
+}
